bd8ecefa0c470d317efd10aa93067ea313e157aa
Artem Achkasov <artemkryt1800@gmail.com>
2022-11-12 14:54:56 +0200

    js removed window.onload

--- js/main.js
+++ js/main.js
@@ -1,19 +1,18 @@
-window.addEventListener("load", function(){
-    let burgerButton = document.querySelector(".header_burger");
-    let nav = document.querySelector(".navigation");
-    let body = document.querySelector("body");
-    burgerButton.addEventListener("click", function(){        
-        nav.classList.toggle("active");
-        burgerButton.classList.toggle("active");
-        body.classList.toggle("lock");
-    });
 
-    let anchor = document.querySelector('.header__bottom');
-    anchor.addEventListener("click", function(e){
-        e.preventDefault();
-        document.querySelector("#cap").scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
+let burgerButton = document.querySelector(".header_burger");
+let nav = document.querySelector(".navigation");
+let body = document.querySelector("body");
+burgerButton.addEventListener("click", function(){        
+    nav.classList.toggle("active");
+    burgerButton.classList.toggle("active");
+    body.classList.toggle("lock");
+});
+
+let anchor = document.querySelector('.header__bottom');
+anchor.addEventListener("click", function(e){
+    e.preventDefault();
+    document.querySelector("#cap").scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
     });
 });
